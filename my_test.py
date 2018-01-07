e3ec0422710bb9ed0a61114a4ec1a4946151bc8f
amuhammad27 <amuhammad27@gmail.com>
2018-01-07 14:29:59 -0600

    Add files via upload

---
+++ my_test.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan  5 15:17:23 2018
+
+@author: root
+"""
+
+import my_debugger 
+
+debugger = my_debugger.debugger()
+
+debugger.load("C:\\WINDOWS\\system32\\calc.exe")
